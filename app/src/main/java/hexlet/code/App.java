d577f0ffb290cffc2a80cdd6029234b980fb97c8
Alex Pestov <alexpestov89@yandex.ru>
2025-12-09 15:42:59 +0300

    DB_PORT

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -54,7 +54,8 @@ public class App {
     }
 
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System.getenv().getOrDefault("DB_PORT", "7070");
+
         return Integer.valueOf(port);
     }
 
